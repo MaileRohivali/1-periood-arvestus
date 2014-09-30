2d19f2143c38091167da2ca0e155e4688c2a43f3
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-09-30 12:56:28 +0300

    Add if sentense

--- layouts/exercises.php
+++ layouts/exercises.php
@@ -12,10 +12,17 @@
       <section id="site-content">
         <h2>Tingimuslause</h2>
         <!--  Sisesta siia kood -->
+        <?php $number = rand(100 , 200);
+          if ($number < 150) {
+            echo "Juhuslik number {$number} on väiksem kui 150";
+          } else {
+            echo "Juhuslik number {$number} on suurem kui 150";
+          }
+        ?>
 
         <h2>Juhuslik pilt</h2>
         <!--  Sisesta siia kood -->
-
+        <img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/1.jpg">
         <h2>Kõikide piltide väljastamine</h2>
         <!--  Sisesta siia kood -->
       </section>
